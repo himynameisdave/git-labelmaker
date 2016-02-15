06ffb658bd48daa416e694dd502c260d0ab41cb2
dlunny <dlunny@parka.ca>
2016-02-15 14:55:49 -0500

    Adds deleteConfirm prompt

---
+++ prompts/deleteConfirm.js
@@ -0,0 +1,10 @@
+/**
+ *    The "are you sure you wanna delete these labels" prompt
+ */
+"use strict";
+module.exports = [{
+  name:     "youSure",
+  type:     "confirm",
+  message:  "Are you sure you want to delete these labels?",
+  default:  true
+}];
