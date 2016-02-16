c52d7ed65075415fa051610132e9916df7d2f6e0
dlunny <dlunny@parka.ca>
2016-02-15 22:33:00 -0500

    Adds & updates some modules
    - Moves the promisified prompt to it's own module
    - moves addPackageValidator to it's own module
    - doCustomLabelPrompts is now a curried/partial applied module/method

--- modules/requestLabels.js
+++ modules/requestLabels.js
@@ -1,6 +1,6 @@
-
 /**
  *    Promisifies the requesting for labels
+ *    @deps   {npm}    octonode - used to request the list of current labels
  *    @param  {String} repo - the working repo
  *    @param  {String} token - the user's GH API token
  *    @return {Promise} - Promise that resolves with the repo's labels, rejects with octonode error
