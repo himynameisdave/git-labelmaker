6fa5be1c8c1bf27add2cd6d1e70098a83c6fee4a
James Monger <jameskmonger@hotmail.co.uk>
2016-02-17 23:26:27 +0000

    Check bcup file exists

--- modules/fetchToken.js
+++ modules/fetchToken.js
@@ -9,12 +9,21 @@ const err = (message) => {
   return { id: "TOKEN", err: message }
 };
 
+const Buttercup = require("buttercup");
+
 module.exports = () => {
-  return new Promise((res, rej)=>{
+  return new Promise((res, rej) => {
+    fs.exists(".git-labelmaker.token.bcup", (exists) => {
+      if (!exists) {
+        rej(err("No token found!"));
+      }
+
+    });
+    /*
     fs.readFile(__dirname+"/../.token.json", 'utf8', (e, data) => {
       if (e || !data) rej(err("No token.json file found!"));
       if (JSON.parse(data).token === "") rej(err("No token found!"))
       res(JSON.parse(data).token);
-    });
+    });*/
   });
 };
