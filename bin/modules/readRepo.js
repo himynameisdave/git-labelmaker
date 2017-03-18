4228c890d638ae3f03d574d272a18d9636edc606
himynameisdave <d@velunny.com>
2017-03-18 14:01:08 -0700

    Cleans up readRepo

--- bin/modules/readRepo.js
+++ bin/modules/readRepo.js
@@ -5,7 +5,7 @@
  *    @return {String}  repo
  */
 'use strict';
-const gitUrl = require('github-url-from-git');
+const getParsedGitUrl = require('./getParsedGitUrl.js');
 const prompt = require('./prompt.js');
 const askWhichRemotePrompt = require('../prompts/askWhichRemote.js');
 const err = require('../utils/errorGenerator')('READ_REPO')('Could not read git repo from your .git/ directory!')
@@ -18,43 +18,25 @@ const throwReadRepoError = () => {
 };
 //  TODO: Break these off too
 const hasMultipleRemotes = (config) => Object.keys(config).filter(key => key.indexOf('remote') > -1);
+
 const hasRemoteUrl = (config, remote) => remote && config[remote] && config[remote]['url'];
 
-module.exports = (config) => {
 
-  return new Promise((res, rej) => {
-    const remotes = hasMultipleRemotes(config);
-    if (!config || !remotes.length) return throwReadRepoError();
+module.exports = (config) => new Promise((res, rej) => {
+  const remotes = hasMultipleRemotes(config);
+  if (!config || !remotes.length) return throwReadRepoError();
 
-    if (remotes.length > 1) {
-        prompt(askWhichRemotePrompt(remotes))
-            .then(({ remote }) => {
-                //  TODO: DRY, this could be broken off
-                const url = config[remote]['url'];
-                const parsedGitUrl = gitUrl(url);
-                //  Note: this is github specific
-                if (!parsedGitUrl && parsedGitUrl.indexOf('https://github.com/') === -1) return throwReadRepoError();
-                res(parsedGitUrl
-                      .split('https://github.com/') // -> ['', 'user/repo']
-                      .reduce((a,b) => b.length ? b : a) // gets the second item in array
-                );
-            })
-            .catch(e => {
-              throwReadRepoError(e);
-              rej(e); // TODO: redundant because throwReadRepoError exits
-            });
-    } else {
-      if (!hasRemoteUrl(config, remotes[0])) return throwReadRepoError();
-      // Retrieve the url of the origin remote
-      // Some repo can have multiple origin
-      const url = config[remotes[0]]['url'];
-      const parsedGitUrl = gitUrl(url);
-      //  Note: this is github specific
-      if (!parsedGitUrl && parsedGitUrl.indexOf('https://github.com/') === -1) return throwReadRepoError();
-      res(parsedGitUrl
-            .split('https://github.com/') // -> ['', 'user/repo']
-            .reduce((a,b) => b.length ? b : a) // gets the second item in array
-      );
-    }
-  });
-};
+  if (remotes.length > 1) {
+      prompt(askWhichRemotePrompt(remotes))
+          .then(({ remote }) => {
+              res(getParsedGitUrl(config, remote, throwReadRepoError));
+          })
+          .catch(e => {
+            throwReadRepoError(e);
+            rej(e); // TODO: redundant because throwReadRepoError exits
+          });
+  } else {
+    if (!hasRemoteUrl(config, remotes[0])) return throwReadRepoError();
+    res(getParsedGitUrl(config, remotes[0], throwReadRepoError));
+  }
+});
