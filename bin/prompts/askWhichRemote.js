8e3801615b81ff345d79a82f39d61a0900962f35
himynameisdave <d@velunny.com>
2017-04-01 15:35:42 -0700

    lintfixes ./bin/prompts/** 🔮🔮🔮

--- bin/prompts/askWhichRemote.js
+++ bin/prompts/askWhichRemote.js
@@ -1,10 +1,9 @@
 /**
  *    The "are you sure you wanna delete these labels" prompt
  */
-`use strict`;
 module.exports = (remotes) => [{
-  name:     `remote`,
-  type:     `list`,
-  message:  `Looks like this repository has multiple remotes.\nWhich one would you like to use?`,
-  choices: remotes
+    name:     'remote',
+    type:     'list',
+    message:  'Looks like this repository has multiple remotes.\nWhich one would you like to use?',
+    choices: remotes,
 }];
